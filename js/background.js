54c688fc8e0b2aa29891b7681a6eec43baf5ce7c
SeongOh Park <42240965+Raoun4136@users.noreply.github.com>
2022-03-15 15:46:21 +0900

    gatsby-theme-init

--- js/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["0.jpeg","4.jpeg","6.jpeg","9.jpeg"];
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-const body = document.querySelector("body");
-
-
-body.style.background = `url("img/${chosenImage}")`;
-body.style.backgroundRepeat = "no-repeat";
-body.style.backgroundSize = "cover";
-body.style.backgroundColor = "black";
